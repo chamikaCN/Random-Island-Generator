9f61471bbe4bf832f845cf1c8aab3a5105a1f348
chamikaCN <chamikanandasiri97@gmail.com>
2020-05-20 18:24:01 +0530

    drawing texture to plane added

---
+++ Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour
+{
+    public Renderer textureRenderer;
+    public void drawTexture(Texture2D texture)
+    {
+        textureRenderer.sharedMaterial.mainTexture = texture;
+        textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
+    }
+}
