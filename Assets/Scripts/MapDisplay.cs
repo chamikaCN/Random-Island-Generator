1bce7def1bb1e01b29e736012475282a0a663bee
chamikaCN <chamikanandasiri97@gmail.com>
2020-07-10 12:11:24 +0530

    added mesh renederer

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class MapDisplay : MonoBehaviour
 {
     public Renderer textureRenderer;
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
+
     public void drawTexture(Texture2D texture)
     {
         textureRenderer.sharedMaterial.mainTexture = texture;
-        textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
+        textureRenderer.transform.localScale = new Vector3(-3*texture.width, 1, 3*texture.height);
+    }
+
+    public void drawMesh(MeshData meshData, Texture2D texture)
+    {
+        meshFilter.sharedMesh = meshData.CreateMesh();
+        meshRenderer.sharedMaterial.mainTexture = texture;
+
+        //meshRenderer.sharedMaterial.SetTexture("_BaseColorMap",texture);
+        //meshRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
     }
 }
